37f680353d9c89d49f9fb28c485cbb56e8fac415
Alex Kotliarskyi <alex.frantic@gmail.com>
2015-08-28 11:53:24 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Instructions
+
+```bash
+$ git clone https://github.com/frantic/currying
+$ node nuts
+```
+
+Your task is to implement `add` function and make sure all
+tests pass.
+
+Have fun :)
---
+++ nuts.js
@@ -0,0 +1,26 @@
+var assert = require('assert');
+
+function add() {
+  // TODO
+}
+
+function test(a, b) {
+  if (a == b) {
+    console.log('OK');
+  } else {
+    console.log(a + ' != ' + b);
+  }
+}
+
+test(add(1, 2), 3);
+test(add(3)(4)(), 7);
+test(add(3)(4)(5), 12);
+
+var three = add(3);
+var four = add(4);
+test(three, 3);
+test(four, 4);
+test(three(5), 8);
+test(three(6), 9);
+test(three(four), 7);
+test(three(four)(three(four)), 14);
